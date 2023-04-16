30253d4407863341b57a866f6b8873487df573b7
Nadeem <shaiknadeem0591@gmail.com>
2023-04-16 16:19:48 +0530

    second commit

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
-
-from flask import Flask, render_template, request
-
-# Create Flask app instance
+from flask import Flask, render_template, request, session, redirect, url_for
 app = Flask(__name__)
-
-# Set up route for the login page
+app.secret_key = "super-secret-key"
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        
-        return 'Logged in successfully!'
+        session['logged_in'] = True
+        return redirect(url_for('dashboard'))
     return render_template('login.html')
-
+@app.route('/dashboard')
+def dashboard():
+    if 'logged_in' in session and session['logged_in'] == True:
+        return render_template('loggedout.html')
+    else:
+        return redirect(url_for('login'))
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    return redirect(url_for('login'))
 if __name__=='__main__':
-    
-
     app.run(debug=True)
